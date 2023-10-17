9d46c1651b8d778987d31f86361066000a0d1a76
unknown <ritikakes33@gmail.com>
2023-10-17 13:14:14 +0530

    learning

---
+++ JAVASCRIPT/ThirtyFive/dom.js
@@ -0,0 +1,9 @@
+//selecting element by getElementById
+
+const toc = document.getElementById("toc")//selecting element and storing in a variable
+console.dir(toc)//displaying
+const banner = document.getElementById("banner")
+console.dir(banner);
+
+//example of exercise:const heading=document.getElementById("mainheading");
+//const image=document.getElementById("unicorn");
