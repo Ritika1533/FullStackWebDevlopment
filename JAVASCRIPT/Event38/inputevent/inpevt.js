fe3c7399910b65006c418cfac18b56dec4b1058e
unknown <ritikakes33@gmail.com>
2023-10-30 10:50:49 +0530

    learning

---
+++ JAVASCRIPT/Event38/inputevent/inpevt.js
@@ -0,0 +1,9 @@
+const input = document.querySelector('input');
+const h1 = document.querySelector('h1');
+//input.addEventListener('change', function () {
+//  console.log("changed");
+//})
+input.addEventListener('input', function (e) {
+    console.log('changed');
+    h1.innerText = input.value;
+})
