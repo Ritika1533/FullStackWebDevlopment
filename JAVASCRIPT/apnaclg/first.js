5147efe7ac3bb546b44986bd689014a83e441d79
unknown <ritikakes33@gmail.com>
2024-02-01 18:23:56 +0530

    practice

---
+++ JAVASCRIPT/apnaclg/first.js
@@ -0,0 +1,35 @@
+console.log("javascript start your journey");
+const price = 99.6;
+let fillName = "Tony Stark";
+let radius = 9;
+console.log(price);
+//arithematic opertor
+let a = 6;
+let b = 9;
+console.log("a=", a, " & b=", b);
+console.log("a+b=", a + b);
+let num = prompt("enter a number:");
+if (num % 5 === 0) {
+    console.log("yes,it is multiple of 5");
+}
+else {
+    console.log("no,it is not multiple of 5");
+}
+let grade = prompt("enter your grade");
+switch (grade) {
+    case 'A':
+        console.log("80-100");
+        break;
+    case 'B':
+        console.log("70-89");
+        break;
+    case 'C':
+        console.log("60-69");
+        break;
+    case 'D':
+        console.log("50-59");
+        break;
+    case 'E':
+        console.log("40-49");
+        break;
+}
