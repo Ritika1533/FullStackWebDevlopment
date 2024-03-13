8eaae1752206c06d959498b21b8718446463c66d
unknown <ritikakes33@gmail.com>
2024-03-13 23:31:19 +0530

    learning

---
+++ PROJECTS/CodeEditor/code-editor/src/components/Cde.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Editor from './Editor'; 
+
+export default function Cde() {
+  return (
+  <>
+   <Editor/>
+   <Editor/>
+   <Editor/>
+  </>
+  )
+}
---
+++ PROJECTS/CodeEditor/code-editor/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './Header'
+import Cde from './Cde'
+
+export default function Home() {
+  return (
+   <>
+      <Header/>
+      <Cde/>
+   </>
+  )
+}
