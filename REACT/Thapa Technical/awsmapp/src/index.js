eb30e2226925c714c1741610fb811eb18aa591e9
unknown <ritikakes33@gmail.com>
2024-07-11 21:53:28 +0530

    react

---
+++ REACT/Thapa Technical/awsmapp/src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDom from "react-dom";
+ReactDom.render(
+	<>
+		<h1>NETFLIX</h1> <p>FAV 5 SHOW</p>
+		<ol>
+			<li>Haseena man jaigi</li>
+			<li>Mirzapur</li>
+			<li>Sukkhie</li>
+			<li>Sabnam ka vedio viral</li>
+			<li>Srikanth</li>
+		</ol>
+	</>,
+	document.getElementById("root")
+);
---
+++ REACT/Thapa Technical/livecovidtracker/src/components/covid.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Covid() {
+	return (
+		<>
+			<h1>🔴 Live</h1>
+			<h2>COVID-19 CORONAVIRUS TRACKER</h2>
+		</>
+	);
+}
+
+export default Covid;
