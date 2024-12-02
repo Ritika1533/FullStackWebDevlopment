ae860da8ea1ddc5b745ed0e57b04950752e403a2
unknown <ritikakes33@gmail.com>
2024-12-02 22:06:19 +0530

    TM

--- PROJECTS/TourMates/tourmate/src/App.js
+++ PROJECTS/TourMates/tourmate/src/App.js
@@ -7,6 +7,7 @@ function App() {
 		<>
 			<Navbar />
 			<Buttons />
+			<HotelCard />
 		</>
 	);
 }
--- PROJECTS/TourMates/tourmate/src/Components/Upcoming Booking/HotelCard.jsx
+++ PROJECTS/TourMates/tourmate/src/Components/Upcoming Booking/HotelCard.jsx
@@ -3,13 +3,21 @@ import React from "react";
 function HotelCard() {
 	return (
 		<div>
-			<nav className="bg-white border-gray-200 text-red-600">
+			<nav className="bg-white border-gray-200 text-sky-800">
 				<div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
 					<p>Booking ID : TMB035365686</p>
 					<p>Vendor Booking ID : TMB035365686</p>
 					<p>Booking Date : Sat. 12 Oct. 2024 14:12:56</p>
 				</div>
 			</nav>
+			<div>
+				<div>
+					<img
+						src="https://media.gettyimages.com/id/493734514/photo/taj-mahal-palace-hotel-in-mumbai-india.jpg?s=612x612&w=gi&k=20&c=NRfRCsWXZqfunQ6amsitPpIjQwM_xdGo0dMFqmov9xM="
+						alt=""
+					/>
+				</div>
+			</div>
 		</div>
 	);
 }
